034e35efdcbd33bf249a3478b57e1b1ea0753e9e
Jie Fan <jxiangfan@gmail.com>
2023-01-13 23:51:48 +1100

    Empty landing page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// This file was automatically generated. Jie commented out all the lines,
+// in case we need to use it in the future.
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Container } from '@mui/material';
+import HeaderBar from '../components/HeaderBar';
+
+const LandingPage = () => {
+  return (
+    <Container>
+      {/* <HeaderBar /> */}
+      <h1> Testing App landing page</h1>
+    </Container>
+  );
+};
+
+export default LandingPage;
